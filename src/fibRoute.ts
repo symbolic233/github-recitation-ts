05846aff923a1b8bb7c131e9ce849d3d4ba553d8
Yoseph Mak <symbolic233@github.com>
2023-01-23 11:45:27 -0500

    made fixes to fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,12 +1,20 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+import { fibonacci } from './fib';
 
-export default (req, res) => {
+type Req = {
+  params: {num: string}
+}
+
+type Res = {
+  send: (result: string) => Promise<String>;
+}
+
+export default (req: Req, res: Res) => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  const fibN : number = fibonacci(parseInt(num));
+  let result: string = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
